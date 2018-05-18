187cee7b06d0a22945c2710442f5a81bc01af6da
Harry <harryle.au@gmail.com>
2018-05-19 03:16:19 +1000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -7,20 +7,31 @@ const {User} = require('./models/user');
 
 const app = express();
 
+// use bodyparser to parse json req to js obj  => can use req.body to get req object
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   const todo = new Todo({
-    text: req.body.text
+    text: req.body.text // can get req.body due to jsonparser
   });
 
   todo.save().then(doc => {
     res.send(doc);
   }, e => {
-    res.send(e);
+    res.status(400).send(e); // send bad status back when error occurs
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => { // find() returns a promise
+    res.send({todos}); // pass todos array back into an obj
+  }, e => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000, () => {
   console.log(`Started on port 3000`);
 });
+
+module.exports = { app };
