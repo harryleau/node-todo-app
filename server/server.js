df6c25288cf206a6393c0a1ab1660a6e87808dcd
Harry <harryle.au@gmail.com>
2018-05-21 02:46:00 +1000

    setup app for heroku, add mongolab add-ons

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // use bodyparser to parse json req to js obj  => can use req.body to get req object
 app.use(bodyParser.json());
@@ -42,15 +43,15 @@ app.get('/todos/:id', (req, res) => {
       if(!todo) {
         return res.status(404).send({});
       } 
-      
+
       res.send({todo});
     })
     .catch(e => res.status(400).send({}));
   
 });
 
-app.listen(3000, () => {
-  console.log(`Started on port 3000`);
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
